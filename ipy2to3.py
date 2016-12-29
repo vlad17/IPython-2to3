2d0daf0495a5c43aff892807684d8c7505779548
Shimon Doodkin <helpmepro1@gmail.com>
2016-12-29 21:23:43 +0200

    better format names, work even with errors, add python3 version to converted file

--- ipy2to3.py
+++ ipy2to3.py
@@ -5,18 +5,28 @@ import io
 import json
 import os
 import subprocess
+import sys
 import tempfile
 
 
 def convert_ipy2to3(json_to_convert):
     if 'worksheets' in json_to_convert:  # json notebook format v3
+        errors = ""
         for worksheet in json_to_convert['worksheets']:
-            convert_a_worksheet_ipy2to3(worksheet, "v3")
-        return ""
+            error = convert_a_worksheet_ipy2to3(worksheet, "v3")
+
+            if error is not "":
+                if errors is not "":
+                    errors += "\r\n\r\n\r\n\r\n"
+                errors += error
+
+        return errors
+
     elif 'cells' in json_to_convert:
-        convert_a_worksheet_ipy2to3(json_to_convert, "v4")
-        return ""
-    return "no cells or worksheets found in the json of the file"
+        return convert_a_worksheet_ipy2to3(json_to_convert, "v4")
+
+    return "notebook-format-error: no cells or worksheets found in the json of the file"
+
 
 def is_python_code_cell_v3(cell):
     return cell['cell_type'] == "code" and cell['language'] == "python"
@@ -32,8 +42,14 @@ def convert_a_worksheet_ipy2to3(worksheet, version):
     else:
         code_cells = filter(is_python_code_cell_v3, worksheet['cells'])
 
+    errors = ""
     for cell in code_cells:
-        convert_a_cell_ipy2to3(cell, version)
+        error = convert_a_cell_ipy2to3(cell, version)
+        if error is not "":
+            if errors is not "":
+                errors += "\r\n\r\n"
+            errors += error
+    return errors
 
 
 def is_magic(line):
@@ -65,18 +81,23 @@ def convert_a_cell_ipy2to3(cell, version):
 
     # convert python2 to python3 by executing 2to3:
     # cmd2to3 = ["C:/Anaconda3_64/Scripts/2to3.exe", "--nobackups", "--write", file_name]  # windows example
-    cmd2to3 = ["2to3", "--nobackups", "--write", file_name]
-    with io.open(os.devnull, "w") as nulls:
-        subprocess.check_call(cmd2to3, stdout=nulls, stderr=nulls)
+    try:
+        cmd2to3 = ["2to3", "--nobackups", "--write", file_name]
+        with io.open(os.devnull, "w") as nulls:
+            subprocess.check_call(cmd2to3, stdout=nulls, stderr=nulls)
 
-    # read converted file back to cell
-    with io.open(file_name, mode="r") as istream:
-        cell['source' if version is "v4" else 'input'] = istream.readlines()
+        # read converted file back to cell
+        with io.open(file_name, mode="r") as istream:
+            cell['source' if version is "v4" else 'input'] = istream.readlines()
 
-    # remove the file
-    os.remove(file_name)
-    for i, line in magic:
-        cell['source' if version is "v4" else 'input'][i] = line
+        # remove the file
+        os.remove(file_name)
+        for i, line in magic:
+            cell['source' if version is "v4" else 'input'][i] = line
+        return ""
+    except subprocess.CalledProcessError as e:
+        return "error converting cell, cell left unconverted: \r\n" + "".join(
+            cell['source' if version is "v4" else 'input'])
 
 
 def main(argv):
@@ -87,16 +108,54 @@ def main(argv):
     with io.open(argv[1], mode="r") as istream:
         ipy_json = json.load(istream)
 
-    error=convert_ipy2to3(ipy_json)
+    error = convert_ipy2to3(ipy_json)
+
     if error is not "":
         print(error)
-        return 1
+        if error.startswith("notebook-format-error"):
+            return 1
+
+    # took the folowing from saving a notebook with python3:
+
+    version3example = """
+{
+"metadata": {
+  "kernelspec": {
+   "display_name": "Python [default]",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.5.2"
+  }
+ }
+}
+    """
+
+    # add version information
+    if 'metadata' in ipy_json:
+        version3example_json = json.loads(version3example)
+        this_version = str(sys.version_info.major) + '.' + str(sys.version_info.minor) + '.' + str(
+            sys.version_info.micro)
+        if 'kernelspec' in ipy_json['metadata']:
+            ipy_json['metadata']['kernelspec'] = version3example_json['metadata']['kernelspec']
+        if 'language_info' in ipy_json['metadata']:
+            ipy_json['metadata']['language_info'] = version3example_json['metadata']['language_info']
+            ipy_json['metadata']['language_info']['version'] = this_version
 
     with io.open(argv[2], mode="w") as ostream:
-        json.dump(ipy_json, ostream)
+        json.dump(ipy_json, ostream, indent=" ")
     return 0
 
 
 if __name__ == "__main__":
-    import sys
     sys.exit(main(sys.argv))
